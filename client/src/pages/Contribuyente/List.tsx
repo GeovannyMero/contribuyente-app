4915ad04bd086c2ff06bf0a1f2cbafe42a820537
gmero <gmero@naturisa.com.ec>
2025-11-26 00:23:36 -0500

    feat: api

--- client/src/pages/Contribuyente/List.tsx
+++ client/src/pages/Contribuyente/List.tsx
@@ -1,10 +1,48 @@
+import { useEffect } from "react";
 import { useParams } from "react-router"
+import { contribuyente } from "../../Types/Contribuyentes.d";
 
+type ListaContri = contribuyente[]
 
 const List: React.FC = () => {
     const params = useParams()
-    console.log(params)
+    console.log("http://127.0.0.1:3000/api/v1/contribuyentes?page=1&limit=5")
     const provinceName = params.province_name;
+
+
+    async function data (): Promise<ListaContri> {
+        
+
+        try {
+            var httpResponse = await fetch("http://127.0.0.1:3000/api/v1/contribuyentes?page=1&limit=5")
+
+            if (!httpResponse.ok)
+                throw new Error(`Error HTTP: ${httpResponse.status}`);
+
+            // 3. Convertir la respuesta a JSON
+            const datos = await httpResponse.json();
+
+            const d: ListaContri = datos['value'] as ListaContri;
+            // 4. Usar los datos
+            console.log('Datos recibidos:', datos);
+            console.log('Datos recibidos:', datos['values']);
+
+            return d;
+        } catch (error) {
+            console.error('Hubo un problema con la petición fetch:', error);
+            return []
+        }
+
+
+    }
+
+    useEffect(() => {
+        data().then(d => {
+            console.log(d)
+        })
+    }, [])
+
+
     return (
         <>
             <h1>
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -8,22 +8,9 @@ import (
 func UserRoutes(app *fiber.App, h *handlers.ContribuyenteHandler) {
 	api := app.Group("/api/v1")
 	api.Get("/contribuyentes", h.Get)
-
-	// api.Static("/", "./client/dist")
 }
 
 // InitStaticRoutes configura el enrutamiento para archivos estáticos.
 func InitStaticRoutes(app *fiber.App) {
-	// fiber.Static configura Fiber para servir archivos desde una ruta del sistema.
-
-	// 1. Ruta URL: /static
-	//    Cualquier solicitud que comience con /static (ej: /static/imagen.jpg)
-
-	// 2. Directorio Local: ./web/static
-	//    Fiber buscará el archivo solicitado en esta carpeta.
-
 	app.Static("/", "../../client/dist")
-
-	// Opcional: Si quieres que el directorio raíz de la API sirva tu frontend, usa "/"
-	// app.Static("/", "./web/static/html")
 }
