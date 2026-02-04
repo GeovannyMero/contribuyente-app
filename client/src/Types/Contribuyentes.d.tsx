308140ba23a2c46eb3ad47a5134a31563fa5d5b1
gmero <gmero@naturisa.com.ec>
2026-02-04 08:51:05 -0500

    fix: mejoras

--- client/src/Types/Contribuyentes.d.tsx
+++ client/src/Types/Contribuyentes.d.tsx
@@ -1,3 +1,8 @@
-export interface contribuyente{
-    Ruc: string
+export interface contribuyente {
+    ruc: string
+    razon_social: string
+    tipo_contribuyente: string
+    codigo_ciiu: string
+    actividad_economica: string
+    estado_contribuyente: string
 }
--- client/src/pages/Contribuyente/List.tsx
+++ client/src/pages/Contribuyente/List.tsx
@@ -9,12 +9,13 @@ const List: React.FC = () => {
     const params = useParams()
     console.log(params)
     console.log("http://127.0.0.1:3000/api/v1/contribuyentes?page=1&limit=5")
-    const provinceName = params.province_name;
+    // const provinceName = params.province_name;
 
-    const[person, setPerson] = useState<contribuyente[]>([])
+    const [person, setPerson] = useState<contribuyente[]>([])
+    const [provinceName, setProvinceName] = useState(params.name);
 
 
-    async function data (): Promise<ListaContri> {
+    async function data(): Promise<ListaContri> {
         try {
             var httpResponse = await fetch("http://127.0.0.1:3000/api/v1/contribuyentes?page=1&limit=5")
 
@@ -24,12 +25,12 @@ const List: React.FC = () => {
             // 3. Convertir la respuesta a JSON
             const datos = await httpResponse.json();
 
-            const d: ListaContri = datos['value'] as ListaContri;
+            const d: ListaContri = datos['Value'] as ListaContri;
             // 4. Usar los datos
             console.log('Datos recibidos:', datos);
             console.log('Datos recibidos:', datos['Value']);
 
-            return datos['Value'];
+            return d;
         } catch (error) {
             console.error('Hubo un problema con la petición fetch:', error);
             return []
@@ -48,13 +49,58 @@ const List: React.FC = () => {
 
     return (
         <>
-            <h1>
-                {provinceName}
-            </h1>
-
-        {person.map(e => {
+            {/* {person.map(e => {
             return <p>{e.razon_social}</p>
-        })}
+        })} */}
+
+            <div className="card w-full h-full bg-base-100 shadow-sm">
+                <div className="card-body">
+                    <div className="card-title text-primary">
+                        { `Provincia de ${provinceName?.toUpperCase()}` }
+                    </div>
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Ruc</th>
+                                    <th>Rasón Social</th>
+                                    <th>Tipo</th>
+                                    <th>Actividad Economíca</th>
+                                    <th>Estado</th>
+                                    <th>Aciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    person.map(e => {
+                                        return (
+                                            <tr key={e.ruc}>
+                                                <th>{e.ruc}</th>
+                                                <td>{e.razon_social}</td>
+                                                <td>{e.tipo_contribuyente}</td>
+                                                <td>{`${e.codigo_ciiu} - ${e.actividad_economica}`}</td>
+                                                <td>{e.estado_contribuyente}</td>
+                                                <td>
+                                                    <button
+                                                        className="btn btn-xs btn-outline btn-info"
+                                                        onClick={() => alert(e.ruc)}>
+                                                        Ver
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="join">
+                        <button className="join-item btn">«</button>
+                        <button className="join-item btn">Page 22</button>
+                        <button className="join-item btn">»</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
